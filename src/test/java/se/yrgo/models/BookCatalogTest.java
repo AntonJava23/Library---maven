33f7a25c8d1e98a11847f48f61871370e2d5a890
AntonJava23 <antbri1009@skola.goteborg.se>
2024-01-26 16:58:50 +0100

    Kodade färdigt testAddBook

--- src/test/java/se/yrgo/models/BookCatalogTest.java
+++ src/test/java/se/yrgo/models/BookCatalogTest.java
@@ -2,48 +2,52 @@ package se.yrgo.models;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class BookCatalogTest {
 
-	private BookCatalog bc;
-	private Book book1;
+    private BookCatalog bc;
+    private Book book1;
 
-	public BookCatalogTest() {
-		bc = new BookCatalog();
-		Book book1 = new Book(1,"Learning Java","","","",0);
-		bc.addBook(book1);
-	}
+    public BookCatalogTest() {
+        bc = new BookCatalog();
+        Book book1 = new Book(1, "Learning Java", "", "", "", 0);
+        bc.addBook(book1);
+    }
 
-	//G
-	@Test
-	public void testAddABook() {
+    //G
+    @Test
+    public void testAddABook() {
+        Book book2 = new Book(3, "Old man and the sea", "Hemmingway", "233436364563", "gg", 237);
+        bc.addBook(book2);
+        assertNotEquals(book2, bc.getBookArray()[0]);
+        assertEquals(book2, bc.getBookArray()[1]);
+    }
 
-	}
+    //G
+    @Test
+    public void testFindBook() {
 
-	//G
-	@Test
-	public void testFindBook() {
+    }
 
-	}
+    //G
+    @Test
+    public void testFindBookIgnoringCase() {
 
-	//G
-	@Test
-	public void testFindBookIgnoringCase() {
+    }
 
-	}
+    //G
+    @Test
+    public void testFindBookWithExtraSpaces() {
 
-	//G
-	@Test
-	public void testFindBookWithExtraSpaces() {
+    }
 
-	}
+    //VG
+    // This test should throw BookNotFoundException in order to pass.
+    @Test
+    public void testFindBookThatDoesntExist() throws BookNotFoundException {
 
-	//VG
-	// This test should throw BookNotFoundException in order to pass.
-	@Test
-	public void testFindBookThatDoesntExist() throws BookNotFoundException {
-
-	}
+    }
 
 }
