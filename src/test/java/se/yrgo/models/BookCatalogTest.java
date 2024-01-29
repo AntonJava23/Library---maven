5076bf0c13c1a356a9bf4fbc1f08eb8abd76be4b
AntonJava23 <antbri1009@skola.goteborg.se>
2024-01-29 14:11:50 +0100

    Finishing tests in bookcatalog and making some in booktest.

--- src/test/java/se/yrgo/models/BookCatalogTest.java
+++ src/test/java/se/yrgo/models/BookCatalogTest.java
@@ -2,8 +2,7 @@ package se.yrgo.models;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookCatalogTest {
 
@@ -27,27 +26,39 @@ public class BookCatalogTest {
 
     //G
     @Test
-    public void testFindBook() {
-
+    public void testFindBook() throws BookNotFoundException {
+        Book book2 = new Book(3, "Old man and the sea", "Hemmingway", "233436364563", "gg", 237);
+        bc.addBook(book2);
+        assertEquals(book2, bc.findBook("Old man and the sea"));
+        assertNotEquals(book1, bc.findBook("Old man and the sea"));
     }
 
     //G
     @Test
-    public void testFindBookIgnoringCase() {
-
+    public void testFindBookIgnoringCase() throws BookNotFoundException {
+        Book book2 = new Book(3, "Old man and the sea", "Hemmingway", "233436364563", "gg", 237);
+        bc.addBook(book2);
+        assertEquals(book2, bc.findBook("Old MAN and tHe sea"));
+        assertNotEquals(book1, bc.findBook("OLD man and the SEA"));
     }
 
     //G
     @Test
-    public void testFindBookWithExtraSpaces() {
-
+    public void testFindBookWithExtraSpaces() throws BookNotFoundException {
+        Book book2 = new Book(3, "Old man and the sea", "Hemmingway", "233436364563", "gg", 237);
+        bc.addBook(book2);
+        assertEquals(book2, bc.findBook("Old      man   and the sea".replaceAll("\\s+", " ")));
+        assertThrows(BookNotFoundException.class, () ->  bc.findBook("Old      man   and the sea"));
     }
 
     //VG
     // This test should throw BookNotFoundException in order to pass.
     @Test
     public void testFindBookThatDoesntExist() throws BookNotFoundException {
-
+        Book book2 = new Book(3, "Old man and the sea", "Hemmingway", "233436364563", "gg", 237);
+        bc.addBook(book2);
+        assertThrows(BookNotFoundException.class, () ->  bc.findBook("Cosmos"));
+        assertDoesNotThrow(() -> bc.findBook("Old man and the sea"));
     }
 
 }
